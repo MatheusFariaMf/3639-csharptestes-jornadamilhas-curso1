14f4062d2c535e988a6596f68a169c82b37741fc
MatheusFariaMf <matheus.mfd@hotmail.com>
2024-10-11 22:40:40 -0300

    UsandoDotnetStryker
    Evolução do curso com testes usando vários casos em uma função e uso da biblioteca dotnet stryker

--- JornadaMilhas.Test/OfertaViagemConstrutor.cs
+++ JornadaMilhas.Test/OfertaViagemConstrutor.cs
@@ -4,31 +4,39 @@ namespace JornadaMilhas.Test
 {
     public class OfertaViagemConstrutor
     {
-        [Fact]
-        public void RetornaOfertaValidaQuandoDadosValidos()
+        [Theory]
+        [InlineData("", null, "2024-01-01", "2024-01-05", 0, false)]
+        [InlineData("OrigemTeste", "DestinoTeste", "2024-02-01", "2024-02-05", 100.0, true)]
+        [InlineData("Lafaiete", "Carrancas", "2024-02-07", "2024-02-01", 100.0, false)]
+        [InlineData("OrigemTeste", "DestinoTeste", "2024-02-01", "2024-02-05", -100.0, false)]
+        public void RetornaEhValidoDeAcordoComDadosEntrada(string origem, string destino, string dataIda,
+            string dataVolta, double preco, bool validacao)
         {
-            // Cenário
-            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
-            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
-            double preco = 100.0;
-            var validacao = true;
-            // Ação
+            // Cenário - arrange
+            Rota rota = new Rota(origem, destino);
+            Periodo periodo = new Periodo(DateTime.Parse(dataIda), DateTime.Parse(dataVolta));
+            // Ação - act
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
-            // Validação
+            // Validação - assert
             Assert.Equal(validacao, oferta.EhValido);
         }
 
-        [Fact]
-        public void RetornaMsgErroQuandoRotaNula()
+        [Theory]
+        [InlineData( "", "","A oferta de viagem não possui rota ou período válidos.")]
+        [InlineData("", "destinozada", "A oferta de viagem não possui Origem da rota válida.")]
+        [InlineData("origemzada", "", "A oferta de viagem não possui Destino da rota válida.")]
+        public void RetornaMsgErroQuandoRotaNulaOuStringVazia(string origem, string destino, string msgErro)
         {
             // Cenário - arrange
-            Rota rota = null;
+            Rota rota = new Rota(origem, destino);
+            if (origem == "" && destino == "")
+                rota = null;
             Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
             double preco = 100.0;
             // Ação - act
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
             // Validação - assert
-            Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+            Assert.Contains(msgErro, oferta.Erros.Sumario);
             Assert.False(oferta.EhValido);
         }
 
@@ -45,18 +53,37 @@ namespace JornadaMilhas.Test
             Assert.False(oferta.EhValido);
         }
 
-        [Fact]
-        public void RetornaMsgErroQuandoPrecoMenorQueZero()
+        [Theory]
+        [InlineData("Carrancas", "Lafaiete", "2024-02-01", "2024-02-05", 100.0, "")]
+        [InlineData("Carrancas", "Lafaiete", "2024-02-01", "2024-02-05", -100.0, "O preço da oferta de viagem deve ser maior que zero.")]
+        [InlineData("Carrancas", "Lafaiete", "2024-02-01", "2024-02-05", 0, "O preço da oferta de viagem deve ser maior que zero.")]
+        public void RetornaSeHouveMsgErroDePrecoMenorOuIgualZero(string origem, string destino, string dataIni,
+            string dataFim, double preco, string fraseErro)
         {
             // Cenário - arrange
-            Rota rota = new Rota("Lafaiete", "Carrancas");
-            Periodo periodo = new Periodo(new DateTime(2024, 9, 26), new DateTime(2024, 9, 30));
-            double preco = -500.0;
+            Rota rota = new Rota(origem, destino);
+            Periodo periodo = new Periodo(DateTime.Parse(dataIni), DateTime.Parse(dataFim));
             // Ação - act
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
-            // Validacao
-            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.",
+            // Validacao - assert
+            Assert.Contains(fraseErro,
                 oferta.Erros.Sumario);
+        }
+
+        [Fact]
+        public void RetornaQuatroErrosDeValidacaoQuandoRotaPeriodoEPrecoSaoInvalidos()
+        {
+
+            // Cenário - arrange
+            Rota rota = new Rota("", "");
+            Periodo periodo = new Periodo(new DateTime(2024, 9, 26), new DateTime(2024, 9, 21));
+            double preco = -500.0;
+            int quantidadeEsperada = 4;
+            // Ação - act
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            // Validacao - assert
+            Assert.Equal(quantidadeEsperada, oferta.Erros.Count());
         }
     }
 }
---
+++ JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,65 @@
+﻿using JornadaMilhasV1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemDesconto
+    {
+        [Fact]
+        public void RetornaPrecoAtualizadoQuandoAplicadoDesconto()
+        {
+            // Arrange - cenário
+            Rota rota = new Rota("Lafaiete", "Carrancas");
+            Periodo periodo = new Periodo(new DateTime(2024, 9, 26), new DateTime(2024, 9, 21));
+            double precoOriginal = 500.0;
+            double desconto = 20;
+            double precoComDesconto = precoOriginal - desconto;
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+            // Act - Ação
+            oferta.Desconto = desconto;
+            // Assert - Validação
+            Assert.Equal(precoComDesconto, oferta.Preco);
+        }
+
+        [Theory]
+        [InlineData(550)]
+        [InlineData(1000)]
+        [InlineData(500)]
+        public void RetornaPrecoComSetentaPorCentoDescontoQuandoDescontoMaiorOuIgualPrecoOriginal(int desconto)
+        {
+            // Arrange - cenário
+            Rota rota = new Rota("Lafaiete", "Carrancas");
+            Periodo periodo = new Periodo(new DateTime(2024, 9, 26), new DateTime(2024, 9, 21));
+            double precoOriginal = 500.0;
+            double precoComDesconto = 500 - 500 * 0.7;
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+            // Act - Ação
+            oferta.Desconto = desconto;
+            // Assert - Validação
+            Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
+        }
+
+        [Theory]
+        [InlineData(-20)]
+        [InlineData(-1)]
+        [InlineData(-500)]
+        [InlineData(-2034)]
+        [InlineData(0)]
+        public void RetornaPrecoOriginalQuandoDescontoForNegativoOuZero(int desconto)
+        {
+            // Arrange - cenário
+            Rota rota = new Rota("Lafaiete", "Carrancas");
+            Periodo periodo = new Periodo(new DateTime(2024, 9, 20), new DateTime(2024, 9, 21));
+            double precoOriginal = 500.0;
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+            // Act - Ação
+            oferta.Desconto = desconto;
+            // Assert - Validação
+            Assert.Equal(precoOriginal, oferta.Preco);
+        }
+    }
+}
